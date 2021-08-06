4fd7bb6f0742f4a4ebc93bc32a251037cca010f2
SachinthaLakshan <sachintha.kahagalla@gmail.com>
2021-08-06 18:17:14 +0530

    added mongodb and user modal

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,14 @@
 import express from 'express';
 import data from './data.js';
+import mongoose from 'mongoose';
+import userRouter from './routers/userRouter.js';
 
 const app = express();
+mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/onlineosu', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
 app.get('/api/product/:id', (req, res) => {
   const product = data.products.find((x) => x._id === req.params.id);
   if (product) {
@@ -13,8 +20,12 @@ app.get('/api/product/:id', (req, res) => {
 app.get('/api/products', (req, res) => {
   res.send(data.products);
 });
+app.use('/api/users', userRouter);
 app.get('/', (req, res) => {
   res.send('Server is ready');
+});
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
 });
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
