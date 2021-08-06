d4e09c01bae54c4b6fbdde6b38b510e4ca82b1da
SachinthaLakshan <sachintha.kahagalla@gmail.com>
2021-08-07 02:58:18 +0530

    auth done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,20 @@ import { BrowserRouter, Route, Link } from 'react-router-dom';
 import CartScreen from './screens/CartScreen';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import SigninScreen from './screens/SigninScreen';
+import { signout } from './actions/userActions';
 
 function App() {
   const cart = useSelector((state) => state.cart);
   const { cartItems } = cart;
+  const userSignin = useSelector((state) => state.userSignin);
+  const { userInfo } = userSignin;
+  const dispatch = useDispatch();
+  const signoutHandler = () => {
+    dispatch(signout());
+  };
+
   return (
     <BrowserRouter>
       <div className="grid-container">
@@ -18,11 +26,27 @@ function App() {
             </Link>
           </div>
           <div>
-            <Link to="/cart">Cart</Link>
-            {cartItems.length > 0 && (
-              <span className="badge">{cartItems.length}</span>
+            <Link to="/cart">
+              Cart
+              {cartItems.length > 0 && (
+                <span className="badge">{cartItems.length}</span>
+              )}
+            </Link>
+            {userInfo ? (
+              <div className="dropdown">
+                <Link to="#">
+                  {userInfo.name}
+                  <i className="fa fa-caret-down"></i>{' '}
+                </Link>
+                <ul className="dropdown-content">
+                  <Link to="#signout" onClick={signoutHandler}>
+                    Sign out
+                  </Link>
+                </ul>
+              </div>
+            ) : (
+              <Link to="/signin">Sign In</Link>
             )}
-            <Link to="/signin">Sign In</Link>
           </div>
         </header>
         <main>
--- frontend/src/screens/SigninScreen.js
+++ frontend/src/screens/SigninScreen.js
@@ -1,19 +1,40 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { signin } from '../actions/userActions';
+import LodingBox from '../components/LodingBox';
+import MessageBox from '../components/MessageBox';
 
-export default function SigninScreen() {
-  const submitHandler = (e) => {
-    e.preventDefault();
-    //action
-  };
+export default function SigninScreen(props) {
+  const dispatch = useDispatch();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+
+  const redirect = props.location.search
+    ? props.location.search.split('=')[1]
+    : '/';
+
+  const userSignin = useSelector((state) => state.userSignin);
+  const { userInfo, loading, error } = userSignin;
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    dispatch(signin(email, password));
+  };
+  useEffect(() => {
+    if (userInfo) {
+      props.history.push(redirect);
+    }
+  }, [props.history, redirect, userInfo]);
+
   return (
     <div className="card">
       <form className="form" onSubmit={submitHandler}>
         <div>
           <h1>Sign In</h1>
         </div>
+        {loading && <LodingBox></LodingBox>}
+        {error && <MessageBox variant="danger">{error}</MessageBox>}
         <div>
           <label htmlFor="email">Email address</label>
           <input
