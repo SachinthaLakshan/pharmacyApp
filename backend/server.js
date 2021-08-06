a157a8570d42db2e23600ef67f51457378c69449
SachinthaLakshan <sachintha.kahagalla@gmail.com>
2021-08-07 01:18:00 +0530

    user auth backend

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,23 @@
 import express from 'express';
-import data from './data.js';
 import mongoose from 'mongoose';
 import userRouter from './routers/userRouter.js';
+import productRouter from './routers/productRouter.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/onlineosu', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
 });
-app.get('/api/product/:id', (req, res) => {
-  const product = data.products.find((x) => x._id === req.params.id);
-  if (product) {
-    res.send(product);
-  } else {
-    res.status(404).send({ message: 'Product not Found' });
-  }
-});
-app.get('/api/products', (req, res) => {
-  res.send(data.products);
-});
+
 app.use('/api/users', userRouter);
+app.use('/api/products', productRouter);
 app.get('/', (req, res) => {
   res.send('Server is ready');
 });
