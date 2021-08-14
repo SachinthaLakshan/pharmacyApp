b20498ba4c4a0df3ffafa302c517a0af1231cf09
SachinthaLakshan <sachintha.kahagalla@gmail.com>
2021-08-15 00:22:23 +0530

    added seller view

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -12,8 +12,8 @@ export default function HomeScreen() {
   const { loading, error, products } = productList;
 
   useEffect(() => {
-    dispatch(listProducts());
-  }, []);
+    dispatch(listProducts({}));
+  }, [dispatch]);
 
   return (
     <div>
